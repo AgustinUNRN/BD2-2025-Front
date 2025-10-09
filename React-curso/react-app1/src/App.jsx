ad232c758b2ee20e037f39a993b8e5f0cb7ac62f
agustin <agustin2345678@gmail.com>
2025-10-09 20:51:50 -0300

    Agregar componente Persona y configurar eslint en Vite

--- React-curso/react-app1/src/App.jsx
+++ React-curso/react-app1/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Persona from './Persona.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -53,6 +54,7 @@ function App() {
 
   return (
     <>
+    <Persona />
      <b>Hola Mundo ! mi primer proyecto en React!</b>
 
      <div>
