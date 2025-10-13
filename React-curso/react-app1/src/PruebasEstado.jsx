8b51a8ad2bdc882d0d957a2d0d7a93fa249c7f47
agustin <agustin2345678@gmail.com>
2025-10-13 12:01:19 -0300

    Tare clase 10 - paginacion cambio de pagina dentro del handler

--- React-curso/react-app1/src/PruebasEstado.jsx
+++ React-curso/react-app1/src/PruebasEstado.jsx
@@ -10,34 +10,80 @@ finamente un botón que permita limpiar ambas tablas.*/
 export default function PruebasEstado() {
 
 const [users, setUsers] = useState([]);
-
 const [posts, setPosts] = useState([]);
+const [totalPagesUsers, setTotalPagesUsers] = useState(1);
+const [totalPagesPosts, setTotalPagesPosts] = useState(1);
+const [pageUser, setPageUser] = useState(1);
+const [pagePost, setPagePost] = useState(1);
 
 useEffect(() => {
   console.log('se llamó al callback de useEffect + clase 9');
-  fetch('https://jsonplaceholder.typicode.com/users')
+  fetch('https://jsonplaceholder.typicode.com/users?_start=0&_limit=10')
         .then((response) => response.json())
-        .then((json) => setUsers(json));
-  fetch('https://jsonplaceholder.typicode.com/posts')
+        .then((json) =>{ setUsers(json); const totalPageU=json.length; setTotalPagesUsers(totalPageU), setPageUser(totalPageU),setTotalPagesUsers(totalPageU);});
+  fetch('https://jsonplaceholder.typicode.com/posts?_start=0&_limit=10')
     .then((response) => response.json())
-    .then((json) => setPosts(json));
+    .then((json) =>{ setPosts(json); const totalPageP=json.length; setTotalPagesPosts(totalPageP), setPagePost(totalPageP),setTotalPagesPosts(totalPageP);});
 }, []);
 
 function handlerOnClickListarPosts() {
-  fetch('https://jsonplaceholder.typicode.com/posts')
+   fetch('https://jsonplaceholder.typicode.com/posts')
     .then((response) => response.json())
-    .then((json) => setPosts(json));
+    .then((json) =>{ setPosts(json); const totalPageP=json.length; setTotalPagesPosts(totalPageP), setPagePost(totalPageP),setTotalPagesPosts(totalPageP);});
+
+    console.log("pagePost= "+pagePost);
 }
 
 function handlerOnClickListarUsuarios() {
-  fetch('https://jsonplaceholder.typicode.com/users')
-    .then((response) => response.json())
-    .then((json) => setUsers(json));
+   fetch('https://jsonplaceholder.typicode.com/users')
+       .then((response) => response.json())
+       .then((json) =>{ setUsers(json); const totalPageU=json.length; setTotalPagesUsers(totalPageU), setPageUser(totalPageU),setTotalPagesUsers(totalPageU);});
+    console.log("pageUser= "+pageUser);
 }
 
 function handlerOnClickLimpiarTablas() {
+  setPageUser(0);
+  setPagePost(0);
   setUsers([]);
   setPosts([]);
+  console.log("pageUser= "+pageUser);
+  console.log("pagePost= "+pagePost);
+}
+
+function handlerClickAgregarUsers() {
+  // Lógica para agregar un nuevo post o usuario
+  fetch('https://jsonplaceholder.typicode.com/users?_limit='+pageUser)
+    .then((response) => response.json())
+    .then((json) => setUsers(json));
+  setPageUser((pageUser) => pageUser + 1);
+  console.log("pageUser= "+pageUser);
+}
+
+function handlerClickSacarUsers() {
+  // Lógica para eliminar un post o usuario
+  fetch('https://jsonplaceholder.typicode.com/users?_limit='+(  pageUser - 1))
+    .then((response) => response.json())
+    .then((json) => setUsers(json));
+  setPageUser((pageUser) => pageUser - 1);
+  console.log("PageUser= "+pageUser);
+}
+
+function handlerClickAgregarPosts() {
+  // Lógica para agregar un nuevo post o usuario
+  fetch('https://jsonplaceholder.typicode.com/posts?_limit='+pagePost)
+    .then((response) => response.json())
+    .then((json) => setPosts(json));
+  setPagePost((pagePost) => pagePost + 1);
+  console.log("PagePost= "+pagePost);
+}
+
+function handlerClickSacarPosts() {
+  // Lógica para eliminar un post o usuario
+  fetch('https://jsonplaceholder.typicode.com/posts?_limit='+(  pagePost - 1))
+    .then((response) => response.json())
+    .then((json) => setPosts(json));
+  setPagePost((pagePost) => pagePost - 1);
+  console.log("PagePost= "+pagePost);
 }
 
   return (
@@ -48,6 +94,12 @@ function handlerOnClickLimpiarTablas() {
         <button onClick={handlerOnClickListarPosts}>Listar posts</button>
         <button onClick={handlerOnClickListarUsuarios}>Listar usuarios</button>
         <button onClick={handlerOnClickLimpiarTablas}>Limpiar tablas</button>
+
+        <h4>Tarea Clase 10</h4>
+        <button disabled={pageUser === totalPagesUsers} onClick={handlerClickAgregarUsers}>Agregar Usuarios a la lista</button>
+        <button disabled={pageUser === 0} onClick={handlerClickSacarUsers}>Eliminar Usuarios de la lista</button>
+        <button disabled={pagePost === totalPagesPosts} onClick={handlerClickAgregarPosts}>Agregar Posts a la lista</button>
+        <button disabled={pagePost === 0} onClick={handlerClickSacarPosts}>Eliminar de la lista de posts</button>
 
         <hr />
         <p>Tabla de Usuarios</p>
--- React-curso/react-app1/src/Usuarios.jsx
+++ React-curso/react-app1/src/Usuarios.jsx
@@ -4,28 +4,44 @@ import './Borde.css'
 export default function Usuarios() {
 
   const [users, setUsers] = useState([]);
+  const [page, setPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(1);
 
   const API_KEY = 'reqres-free-v1'; 
   const API_AUTORIZATION = 'x-api-key'
-  const API_URL = 'https://reqres.in/api/users';
 
   //acá no puedo tener efectos de lado
 
   useEffect(() => {
+    console.log(page);
+  });
+
+  useEffect(() => {
     console.log('se llamó al callback de useEffect');
-    fetch(API_URL,{headers: {[API_AUTORIZATION]: API_KEY}})
+    fetch('https://reqres.in/api/users?page='+page,{headers: {[API_AUTORIZATION]: API_KEY}})
           .then((response) => response.json())
-          .then((json) => setUsers(json.data));
-  }, []);
+          .then((json) => {
+            setTotalPages(json.total_pages);
+            setUsers(json.data);
+          });
+  }, [page]); //importante page para poder paginar
 
   function handlerClickListarUsuarios() {
-    fetch(API_URL,{headers: {[API_AUTORIZATION]: API_KEY}})
+    fetch('https://reqres.in/api/users?page='+page,{headers: {[API_AUTORIZATION]: API_KEY}})
       .then((response) => response.json())
       .then((json) => setUsers(json.data));
   }
 
   function handlerClikBorrarUsuarios() {
     setUsers([]);
+  }
+
+  function handlerClickAnterior() {
+    setPage((page) => page - 1);
+  }
+
+  function handlerClickSiguiente() {
+    setPage((page) => page + 1);
   }
 
 
@@ -37,25 +53,36 @@ export default function Usuarios() {
 
         <button onClick={handlerClickListarUsuarios}>Listar Usuarios</button>
         <button onClick={handlerClikBorrarUsuarios}>Borrar</button>
+
+        <h4>Botones agregados para la Clase 10</h4>
+        <button disabled={page === 1} onClick={handlerClickAnterior}>Anterior</button>
+        <button disabled={page === totalPages} onClick={handlerClickSiguiente}>Siguiente</button>
         <hr />
         <table>
           <thead>
             <tr>
-              <th>ID</th>
-              <th>Nombre</th>
-              <th>Username</th>
-              <th>Email</th>
+                <th>ID</th>
+                <th>Email</th>
+                <th>Nombre</th>
+                <th>Apellido</th>
+                <th>Avatar</th>
             </tr>
           </thead>
           <tbody>
             {users.map((user) => (
-              <tr key={user.id}>
-                <td>{user.id}</td>
-                <td>{user.first_name}</td>
-                <td>{user.last_name}</td>
-                <td>{user.email}</td>
-                <td><img src={user.avatar} alt={user.first_name} /></td>
-              </tr>
+               <tr key={user.id}>
+                  <td>{user.id}</td>
+                  <td>{user.email}</td>
+                  <td>{user.first_name}</td>
+                  <td>{user.last_name}</td>
+                  <td>
+                    <img 
+                      src={user.avatar} 
+                      alt={`${user.first_name} ${user.last_name}`}
+                      style={{width: '50px', height: '50px', borderRadius: '50%'}}
+                    />
+                  </td>
+                </tr>
             ))}
           </tbody>
         </table>
