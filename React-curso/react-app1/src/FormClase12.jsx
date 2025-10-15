2741a05eae72d76c43224ff501ab77a608f34fa0
agustin <agustin2345678@gmail.com>
2025-10-14 22:17:02 -0300

    Refactor FormClase12 to handle form submission via API and manage state with useEffect; update input handling and response display.

--- React-curso/react-app1/src/FormClase12.jsx
+++ React-curso/react-app1/src/FormClase12.jsx
@@ -1,16 +1,46 @@
 //import React from 'react'
-import {useState} from 'react'
+import {useEffect, useState} from 'react'
 import './Borde.css'
 
 
 export default function FormClase12() {
     const [resultado, setResultado] = useState("")
+    const [formData, setFormData] = useState({
+        fname: "",
+        lname: "",
+    })
+
+    const API_KEY = 'reqres-free-v1'; 
+    const API_AUTORIZATION = 'x-api-key'
+
+    //fetch('https://reqres.in/api/users',{headers: {[API_AUTORIZATION]: API_KEY}})
+
+    useEffect(() => {
+        console.log("formData cambió:", formData);
+    })
+
+    function handleChange(event){
+        const inputName = event.target.name;
+        const inputValue = event.target.value;
+        setFormData(formData => ({
+            ...formData,
+            [inputName]: inputValue,
+        }))
+    }
 
     function handleSubmit(event){
-        event.preventDefault()
-        const nombre = event.target.fname.value
-        const apellido = event.target.lname.value
-        setResultado(`Hola ${nombre} ${apellido}, bienvenido al curso de React`)
+        event.preventDefault();//evita que se recargue la página
+        fetch('https://reqres.in/api/users',{
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json; charset=UTF-8',
+                [API_AUTORIZATION]: API_KEY
+            },
+            body: JSON.stringify(formData),
+        })
+        .then(response => response.json())
+        .then(json => setResultado("Creado usuario con ID: " + json.id));
+        
     }
 
   return (
@@ -19,13 +49,13 @@ export default function FormClase12() {
         <form onSubmit={handleSubmit}>
             <label htmlFor="fname">Nombre:</label>
             <br />
-            <input type="text" id='fname' name='fname' />
+            <input type="text" id='fname' name='fname' value={formData.fname} onChange={handleChange} />
             <br />
             <label htmlFor="lname">Apellido:</label>
             <br />
-            <input type="text" id='lname' name='lname'/>
+            <input type="text" id='lname' name='lname' value={formData.lname} onChange={handleChange} />
             <br />
-            <button type="submit">Enviar</button> {/* ✅ Agregué el botón submit */}
+            <button type="submit">Enviar</button>
         </form>
     </div>
 
